b0475e82669b0dbc9da8542aff5d6320f17094fb
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-05-08 15:09:51 -0400

    added survey model and migration

---
+++ db/migrate/20150508145319_create_surveys.rb
@@ -0,0 +1,11 @@
+class CreateSurveys < ActiveRecord::Migration
+  def change
+    create_table :surveys do |t|
+      t.string :title, null: false
+      t.string :picture_url
+      t.integer :user_id, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
