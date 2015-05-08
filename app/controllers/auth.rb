44dcf011d8efb56a916aa603cd1c11c828e6758e
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-05-08 18:20:30 -0400

    create authorization and respec tive views

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,27 @@
 get '/' do
-  erb :welcome
+  erb :index
 end
 
-get '/login' do
-  
+get '/sign_up' do
+  erb :sign_up
 end
 
-get '/signup' do
-  
+post '/log_in' do
+  user = User.find_by(email: params[:user][:email])
+  redirect '/?error=user_not_found' unless user
+  redirect '/?error=password_not_correct' unless user.authenticate?(params[:user][:password])
+  session[:user_id] = user.id
+  redirect "/users/#{user.id}"
+end
+
+post '/sign_up' do
+  user = User.new(params[:new_user])
+  return [500, "Wasn't able to sign up, try again."] unless user && user.save
+  session[:user_id] = user.id
+  redirect "/users/#{user.id}"
+end
+
+post '/log_out' do
+  session[:user_id] = nil
+  redirect '/'
 end
