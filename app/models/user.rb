71ed515c9f4f4c9f5f4f21327f501ae64ec0f572
AlexTaber & HanuMaIV <pair+alextaber.hanumaiv@devbootcamp.com>
2015-05-08 18:52:24 -0400

    debug user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
+  include BCrypt
   has_many :surveys
   has_many :completions
 
   validates :name, presence: true
   validates :email, presence: true
-  validates :password_hash, presence: true
+  # validates :password_hash, presence: true
 
 
   def password
