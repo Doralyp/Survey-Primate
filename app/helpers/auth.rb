da5ec46edaaba426643ee3bba0cb093eab9748aa
Ace Burgess <burgess.ace@gmail.com>
2015-05-09 19:21:30 -0400

    Abstract some logic out to the model

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -9,3 +9,7 @@ end
 def must_be_logged_in
   redirect '/' unless current_user
 end
+
+def must_be_invited survey_id
+  redirect '/' unless !!Completion.find_by(survey_id:survey_id, user_id: current_user.id)
+end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -5,4 +5,19 @@ class Survey < ActiveRecord::Base
   has_many :questions
 
   validates :title, presence: true
+
+  def add_question_to_survey question, choices, survey_id
+    survey = Survey.find(survey_id)
+    new_question = Question.new(question)
+    new_question.survey = survey
+    new_question.save
+    new_choices = choices.values.map do |input|
+      if !!input
+        new_choice = Choice.new(choice: input)
+        new_choice.question = new_question
+        new_choice.save
+      end
+    end
+  end
+
 end
