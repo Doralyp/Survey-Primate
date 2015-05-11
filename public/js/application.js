ed372609fb6217023bc46e8ccc1b428744004391
Brendan Miranda <me@brendanmiranda.com>
2015-05-10 23:23:56 -0400

    Ajax on invite users page

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,19 @@ $(document).ready(function() {
     $('#form-logout').submit();
   });
 
+  $('.btn-invite-user').click(function(e){
+    e.preventDefault();
+      $.ajax({
+          url: this.action,
+          method: this.method,
+          data: $(e.target).serialize()
+        })
+        .done(function(resp){
+          $(e.target).remove();
+        })
+        .error(function(resp){
+          alert(JSON.stringify(resp))
+        })
+    });
+
 });
